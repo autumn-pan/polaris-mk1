7e09f2b7139cc1961b1b6d93b007ff603d0bb00e
Autumn Pan <pan662607@gmail.com>
2025-05-25 13:38:13 -0400

    overhaul SDCard class to Logger

---
+++ src/subsystems/information/logger.cpp
@@ -0,0 +1,95 @@
+#include "logger.h"
+#include <SPI.h>
+#include <SD.h>
+#include "../control/sensors/sensors.h"
+
+Logger::Logger(String notes)
+{
+    //Begin SD card
+    if (!SD.begin(chipSelect))
+    {
+        Serial.println("SD card initialization failed!");
+        return;
+    }
+    Serial.println("SD card initialized successfully.");
+
+    // Open or create the log directory
+    if (!SD.exists("/logs"))
+    {
+        if (SD.mkdir("/logs"))
+        {
+            Serial.println("Log directory created successfully.");
+        }
+    }
+    // Log files are enumerated in chronological order, i.2
+    // log_0.txt, log_1.txt, etc.
+    // Figure out the number of log files already present
+    int logCount = 0;
+    while (SD.exists(("/logs/log_" + String(logCount) + ".txt").c_str()))
+    {
+        logCount++;
+    }
+
+    // Create a new log file
+    String name = "/logs/log_" + String(logCount) + ".txt";
+    dataFile = SD.open(name.c_str(), FILE_WRITE);
+
+    if (!dataFile)
+    {
+        Serial.println("Error opening log file!");
+        return;
+    }
+    Serial.println("Log file created successfully.");
+
+    // Write the header to the log file
+    dataFile.println("POLARIS I FLIGHT LOG" + String(logCount) + "\n");
+    dataFile.println(notes + "\n");
+}
+
+void Logger::log(String data)
+{
+    // Write data to the SD card
+    if (dataFile)
+    {
+        dataFile.println(data);
+    }
+    else
+    {
+        Serial.println("Error writing to the log file");
+        return;
+    }
+
+    // Print data to the serial monitor
+    Serial.println(data);
+}
+
+void Logger::logSensorData()
+{
+    // Log sensor data to the SD card
+    if (dataFile)
+    { //Change these into meaningful information when other systems are implemented
+        dataFile.print("Foo: ");
+        dataFile.print("Bar");
+    }
+    else
+    {
+        Serial.println("Error writing to the log file");
+        return;
+    }
+}
+
+void Logger::kill()
+{
+    // Close the log file
+    if (dataFile)
+    {
+        dataFile.close();
+        Serial.println("Log file closed successfully.");
+    }
+    else
+    {
+        Serial.println("Data file not open");
+        return;
+    }
+}
+
---
+++ src/subsystems/information/logger.h
@@ -0,0 +1,19 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <SD.h>
+#include <SPI.h>
+
+class Logger
+{
+    public:
+        Logger(String notes = ""); // Constructor to initialize the SD card and create a log file
+        void log(String data); // Writes data to the SD card and prints it to the serial monitor
+        void logSensorData(); // Logs sensor data to the SD card
+        void kill(); // Closes the log file
+    private:
+        const int chipSelect = BUILTIN_SDCARD;
+        File dataFile;
+};
+
+#endif
