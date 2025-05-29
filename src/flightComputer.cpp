9e637d8de8aaff2523d730b44b50f77c248d9ab2
Autumn Pan <pan662607@gmail.com>
2025-05-29 17:54:48 -0400

    fix kalman filter

---
+++ src/flightComputer.cpp
@@ -0,0 +1,47 @@
+#include "flightComputer.h"
+#include "subsystems/control/filter.h"
+#include "subsystems/control/timeTracker.h"
+#include "subsystems/control/sensors/sensors.h"
+#include "subsystems/information/logger.h"
+#include "subsystems/io/pyro.h"
+#include "pins.h"
+#include <Arduino.h>
+
+
+FlightComputer::FlightComputer(float processNoise[2], float measurementNoise[2], float seaLevelPressure)
+{
+    // instantiate all flight components
+    timeTracker = new TimeTracker();
+
+    // data logging
+    logger = new Logger("");
+
+    // control instances
+    sensors = new SensorStack(logger);
+    filter = new KalmanFilter(processNoise, measurementNoise, timeTracker);
+
+    // pyro channels
+    pyro1 = new PyroChannel(12, 1000, logger); // 12 is a placeholder pin value
+    pyro2 = new PyroChannel(13, 1000, logger); // 13 is a placeholder pin value
+
+    this->seaLevelPressure = seaLevelPressure;
+}
+
+void FlightComputer::update()
+{
+    // keep track of time
+    timeTracker->update();
+
+    // Update pyro channels if they're active
+    if(pyro1->isFiring())
+        pyro1->update();
+
+    if(pyro2->isFiring())
+        pyro2->update();
+
+    // Update state information
+    sensors->updateSensorData();
+    filter->update(sensors->getAccelY(), sensors->getAltitude(seaLevelPressure));
+
+    // TODO: implement sensor logging for state info
+}
--- src/subsystems/control/filter.cpp
+++ src/subsystems/control/filter.cpp
@@ -5,17 +5,10 @@
 #include "./sensors/sensors.h"
 #include "./timeTracker.h"
 
-KalmanFilter::KalmanFilter(float processNoise[], float measurementNoise[], TimeTracker* timeTracker)
+KalmanFilter::KalmanFilter(float processNoise[], float measurementNoise[], TimeTracker * timeTracker)
 {
-    // First, initialize the time tracker
-    this->timeTracker = timeTracker;
-
-    // Then, initialize dt
-    dt = timeTracker->getTimeStep()/1000.0f; // Convert milliseconds to seconds
-
     // Initialize state vector; estimated initial values
-    x = Eigen::Vector3f::Zero(); // [acceleration, velocity, position]
-    
+    x << 0, 0, 0;    
     // Initialize covariance matrices
     P = Eigen::Matrix3f::Identity();
     Q  <<
@@ -25,29 +18,26 @@ KalmanFilter::KalmanFilter(float processNoise[], float measurementNoise[], TimeT
 
     R <<
         measurementNoise[0], 0, 0,
-        0, 0, 0,
+        0, 1e6, 0,
         0, 0, measurementNoise[1]; // Measurement noise covariance matrix R
     
     // Initialize Kalman gain and identity matrix
     K = Eigen::Matrix3f::Zero();
     I = Eigen::Matrix3f::Identity();
     
-    // State transition matrix (assuming constant acceleration model)
-    F << 1, dt, 0.5*dt * dt,
-         0, 1, dt,
-         0, 0, 1;
-
-    processNoise = processNoise;
-    measurementNoise = measurementNoise;
 }
 
 void KalmanFilter::update(float measuredAcceleration, float measuredPosition)
 {
-    // Before anything, update dt based on the timeTracker
-    dt = timeTracker->getTimeStep() / 1000.0f;
-    
+    dt = timeTracker->getTimeStep() / 1000.0f; // Convert milliseconds to seconds
+
+    // State transition matrix (assuming constant acceleration model)
+    F << 1, 0, 0, // State transition for acceleration
+         dt, 1, 0, // State transition for velocity
+         0.5 * dt * dt, dt, 1; // State transition for position
     // Predict the next state...
     xp = F * x; // no control matrix... yet
+
     P = F * P * F.transpose() + Q; // Update estimate error covariance
     // Measurement vector
     Eigen::Vector3f z;
@@ -73,6 +63,7 @@ float KalmanFilter::getAcceleration()
 }
 float KalmanFilter::getVelocity()
 {
+
     return x(1); // Return estimated velocity
 }
 float KalmanFilter::getPosition()
